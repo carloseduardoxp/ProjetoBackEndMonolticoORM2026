99d62b6a81238c7cb4e5f42d9c663110a03355ee
teste <you@example.com>
2026-04-10 19:53:59 -0300

    Salvando contato controller

--- contatos/src/main/java/br/edu/iftm/pbackorm/contatos/controller/ContatoController.java
+++ contatos/src/main/java/br/edu/iftm/pbackorm/contatos/controller/ContatoController.java
@@ -3,9 +3,12 @@ package br.edu.iftm.pbackorm.contatos.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,26 +51,15 @@ public class ContatoController {
                 .findByNomeContainingIgnoreCaseAndEmailContainingIgnoreCaseAndTelefoneContaining(
                     nome, email, telefone);
     }
-/* 
+
     @PostMapping
-    public ResponseEntity<?> novo(@RequestBody Contato novoContato) {
-        boolean existe = contatos.stream()
-                    .anyMatch(contato -> contato.getCodigo().equals(novoContato.getCodigo()));
-        if (existe) {
-            return ResponseEntity.status(HttpStatus.CONFLICT)
-            .body(new ErroDTO("Já existe contato de código "+novoContato.getCodigo()
-                            ,LocalDateTime.now()));
-        }
-        if (novoContato.getNome() == null || novoContato.getNome().isBlank()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(new ErroDTO("Contato com nome vazio",LocalDateTime.now()));
-        }
-        contatos.add(novoContato);
+    public ResponseEntity<Contato> novo(@RequestBody Contato novoContato) {
+        Contato contato = repository.save(novoContato);
         return ResponseEntity
                     .status(HttpStatus.CREATED)
-                    .body(novoContato);
+                    .body(contato);
     }
-
+/* 
     @PutMapping("/{id}")
     public ResponseEntity<?> atualizar(@PathVariable Integer id,
                                        @RequestBody Contato contatoAtualizado) {
