f899a7d3463cd346b87aa85196a4646fad343595
CArlos Eduardo <carloseduardoxpto@gmail.com>
2026-05-29 22:08:58 -0300

    Usando JPQL e Query nativo

--- e-commerce/src/main/java/br/edu/iftm/tspi/pbackorm/e_commerce/controller/ProdutoController.java
+++ e-commerce/src/main/java/br/edu/iftm/tspi/pbackorm/e_commerce/controller/ProdutoController.java
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.edu.iftm.tspi.pbackorm.e_commerce.domain.Categoria;
 import br.edu.iftm.tspi.pbackorm.e_commerce.domain.Produto;
 import br.edu.iftm.tspi.pbackorm.e_commerce.dto.ProdutoDTO;
 import br.edu.iftm.tspi.pbackorm.e_commerce.dto.mapper.ProdutoMapper;
@@ -21,7 +21,6 @@ import br.edu.iftm.tspi.pbackorm.e_commerce.repository.ProdutoRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PutMapping;
 
 
 
@@ -87,6 +86,24 @@ public class ProdutoController {
                                         ));
         repository.delete(produto);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("maior-preco-por-categoria")
+    public ResponseEntity<List<ProdutoDTO>> buscarProdutosComMaiorPrecoPorCategoria() {
+         List<ProdutoDTO> produtos = repository.buscarProdutosComMaiorPrecoPorCategoria();
+         if (produtos.isEmpty()) {
+            return ResponseEntity.noContent().build();
+         }
+         return ResponseEntity.ok(produtos);
+    }
+
+    @GetMapping("maior-preco-por-categoria-nativo")
+    public ResponseEntity<List<ProdutoDTO>> buscarProdutosComMaiorPrecoPorCategoriaNativo() {
+         List<ProdutoDTO> produtos = repository.buscarProdutosComMaiorPrecoPorCategoriaNativo();
+         if (produtos.isEmpty()) {
+            return ResponseEntity.noContent().build();
+         }
+         return ResponseEntity.ok(produtos);
     }
 
     
--- e-commerce/src/main/java/br/edu/iftm/tspi/pbackorm/e_commerce/repository/ProdutoRepository.java
+++ e-commerce/src/main/java/br/edu/iftm/tspi/pbackorm/e_commerce/repository/ProdutoRepository.java
@@ -3,9 +3,11 @@ package br.edu.iftm.tspi.pbackorm.e_commerce.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.edu.iftm.tspi.pbackorm.e_commerce.domain.Produto;
+import br.edu.iftm.tspi.pbackorm.e_commerce.dto.ProdutoDTO;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto,Integer> {
@@ -13,5 +15,30 @@ public interface ProdutoRepository extends JpaRepository<Produto,Integer> {
     public List<Produto> 
         findByNomeContainingIgnoreCaseAndEstoqueGreaterThanAndPrecoLessThan(
             String nome,Short estoqueMin, Double precoMax);
+
+    @Query("""
+              SELECT new br.edu.iftm.tspi.pbackorm.e_commerce.dto.ProdutoDTO(
+                    p.id,p.nome,p.preco,p.estoque,p.caminhoImagem,p.categoria.id)
+              FROM Produto p
+              WHERE p.preco = (
+                    SELECT MAX(p2.preco)
+                    FROM Produto p2
+                    WHERE p2.categoria.id = p.categoria.id
+                    )      
+            """)
+    public List<ProdutoDTO> buscarProdutosComMaiorPrecoPorCategoria();
+
+
+    @Query(nativeQuery = true, value = """
+            Select p.produtoID,p.produtoNome,p.preco,
+                   p.unidadesemestoque,p.imagem,p.categoriaID
+            FROM Produtos p
+            where p.preco = (
+                    Select MAX(p2.preco)
+                    From Produtos p2
+                    Where p2.categoriaID = p.categoriaID
+                    )
+            """)
+    public List<ProdutoDTO> buscarProdutosComMaiorPrecoPorCategoriaNativo();
 
 }
